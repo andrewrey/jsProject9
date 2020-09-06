56078846e65ad73432b576c3399e55c3254cb0e6
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-06 10:52:02 -0700

    adding initial javascript

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// DOM Elements
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+// Dummy Transactions
+const dummyTransactions = [
+  {id:1, text:'Flower', amount: -20}
+  {id:2, text:'Salary', amount: 300}
+  {id:3, text:'Book', amount: -10}
+  {id:4, text:'Camera', amount: 150}
+];
+
+
+let transations = dummyTransactions;
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction){
+  // Get sign
+  const sign = transaction.amount < 0 ? '-': '+';
+  // Create Element
+  const item = document.createElement('li');
+  // Add class based on value
+  item.classList.add(transaction.amount < 0? 'minus': 'plus');
+
+  item.innerHTML = `
+    ${tranaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn>x</button> 
+  `;
+  list.appendChild(item);
+}
+
+
+// Init app
